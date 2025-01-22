69b432448209afa1bbdfc20d99cf47eb6ecc2028
tejab-titare <51313240+tejab-titare@users.noreply.github.com>
2025-01-22 19:46:40 +0530

    Update SaleStore.java

--- SaleStore.java
+++ SaleStore.java
@@ -1,1 +1,23 @@
+package com.app.productbase.pi;
+
+import java.util.List;
+/**
+ * SaleStore class 
+ * 
+ */
+public class SaleStore {
+
+	List<Integer> paidItems;
+	List<Integer> freeItems;
+
+	/**
+	 * 
+	 * @param paidItems
+	 * @param freeItems
+	 */
+	public SaleStore(List<Integer> paidItems, List<Integer> freeItems) {
+		this.paidItems = paidItems;
+		this.freeItems = freeItems;
+	}
+}
 
